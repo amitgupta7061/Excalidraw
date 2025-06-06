178b921f4ed3c6614693b0a946aecd2bf308bb1e
Amit <amitgupta561710@gmail.com>
2025-06-06 14:02:55 +0530

    added-rectangle

--- apps/draw-frontend/src/app/canvas/[roomId]/page.tsx
+++ apps/draw-frontend/src/app/canvas/[roomId]/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useRef } from 'react'
 
 const Canvas = () => {
@@ -6,14 +7,44 @@ const Canvas = () => {
 
     useEffect(() => {
         if(canvasRef.current){
-            const canvas = canvasRef.current;
-            const ctx = canvas.getContext("2d");
+          const canvas = canvasRef.current;
+          const ctx = canvas.getContext("2d");
+
+          if(!ctx) return;
+
+          let clicked = false, startX = 0, startY = 0;
+          canvas.addEventListener("mousedown", (e) => {
+            clicked = true;
+            startX = e.clientX;
+            startY = e.clientY;
+          })
+
+          canvas.addEventListener("mouseup", (e) => {
+            clicked = false;
+            console.log(e.clientX);
+            console.log(e.clientY);
+          })
+
+          canvas.addEventListener("mousemove", (e) => {
+            if(clicked){
+              const width   = e.clientX - startX;
+              const height  = e.clientY - startY;
+              ctx.clearRect(0, 0, canvas.width, canvas.height);
+              ctx.strokeRect(startX, startY, width, height);
+
+            }
+          })
         }
     }, [canvasRef]);
 
   return (
-    <div>
-        <canvas ref={canvasRef} width={1200} height={1000}></canvas>
+    <div className='text-white'>
+        <canvas
+        ref={canvasRef}
+        width={innerWidth}
+        height={innerHeight}
+        style={{ backgroundColor: "white", display: "block" }}
+      />
     </div>
   )
 }
